f09d377de270630e6012e5f5a687ec95951841fc
Joe <t7051015@live.tees.ac.uk>
2020-10-26 01:19:31 +0000

    Zombie has health bar

--- app/src/main/java/com/example/virusgame/game/Player.kt
+++ app/src/main/java/com/example/virusgame/game/Player.kt
@@ -7,18 +7,25 @@ import android.graphics.Paint
 import androidx.core.content.ContextCompat
 import com.example.virusgame.R
 
-class Player(context: Context){
+class Player(var context: Context){
     private var screenWidth = Resources.getSystem().displayMetrics.widthPixels
     var gold: Int = 0
     var health: Int = 10
     private val goldPaint: Paint = Paint()
+    private val healthPaint: Paint = Paint()
 
     init {
-        goldPaint.textSize = 40.0f
-        goldPaint.color = ContextCompat.getColor(context, R.color.gold)
+        setPaintParams(goldPaint,50.0f, R.color.gold)
+        setPaintParams(healthPaint, 50.0f, R.color.green)
+    }
+
+    private fun setPaintParams(paint: Paint, textSize: Float, colorResource: Int) {
+        paint.textSize = textSize
+        paint.color = ContextCompat.getColor(context, colorResource)
     }
 
     fun draw(canvas: Canvas){
         canvas.drawText("Gold: $gold", screenWidth - 250.0f, 150.0f, goldPaint)
+        canvas.drawText("Health: $health", screenWidth - 250.0f, 100.0f, healthPaint)
     }
 }
--- app/src/main/java/com/example/virusgame/game/Zombie.kt
+++ app/src/main/java/com/example/virusgame/game/Zombie.kt
@@ -8,6 +8,7 @@ import android.graphics.Paint
 import android.graphics.Rect
 import androidx.core.content.ContextCompat
 import com.example.virusgame.R
+import java.lang.Exception
 
 class Zombie(var image: Bitmap, context: Context) : SwipeTaker {
     private var screenWidth = Resources.getSystem().displayMetrics.widthPixels
@@ -15,23 +16,35 @@ class Zombie(var image: Bitmap, context: Context) : SwipeTaker {
     private var x: Int = screenWidth / 2 - image.width / 2
     private var y: Int = screenHeight / 2 - image.height / 2
     override var rect: Rect get() { return Rect(x, y, x + image.width, y + image.height) } set(value) {}
-    var health: Int = 3
+    private var maxHealth: Int = 3
+    private var currentHealth: Int = maxHealth
     private val zombieHealthPaint: Paint = Paint()
 
     init {
-        zombieHealthPaint.textSize = 40.0f
-        zombieHealthPaint.color = ContextCompat.getColor(context, R.color.white)
+        zombieHealthPaint.color = ContextCompat.getColor(context, R.color.red)
     }
 
     fun draw(canvas: Canvas){
         canvas.drawBitmap(image, x.toFloat(), y.toFloat(), null)
-        canvas.drawText("Health: $health", screenWidth - 250.0f, 100.0f, zombieHealthPaint)
+        drawHealthBar(canvas)
+    }
+
+    private fun drawHealthBar(canvas: Canvas) {
+        val healthBarStartPos = x + 50.0f
+        val maxHealthBarStopPos = rect.right - 50.0f
+        val healthBarStopPos: Float = try {
+            (maxHealthBarStopPos - healthBarStartPos) / maxHealth * currentHealth + healthBarStartPos
+        } catch (e: Exception) {
+            healthBarStartPos
+        }
+        val healthBarHeight = y - 50.0f
+        canvas.drawLine(healthBarStartPos, healthBarHeight, healthBarStopPos, healthBarHeight, zombieHealthPaint)
     }
 
     private fun takeDamage(damage: Int) {
-        health -= damage
-        if(health <= 0){
-            health = 0
+        currentHealth -= damage
+        if(currentHealth <= 0){
+            currentHealth = 0
             die()
         }
     }
