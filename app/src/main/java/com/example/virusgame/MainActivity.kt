03546f6fe80bd3fd5dd2d18638ea771d2a0181df
Joe <t7051015@live.tees.ac.uk>
2020-10-26 18:06:26 +0000

    Shake moved to PreAttackZombie

--- app/src/main/java/com/example/virusgame/MainActivity.kt
+++ app/src/main/java/com/example/virusgame/MainActivity.kt
@@ -1,21 +1,11 @@
 package com.example.virusgame
 
-import android.content.Context
-import android.hardware.Sensor
-import android.hardware.SensorManager
 import android.os.Bundle
 import android.view.WindowManager
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.example.virusgame.game.ShakeSensor
-import com.example.virusgame.game.ShakeSensor.OnShakeListener
 
 
 class MainActivity : AppCompatActivity(){
-    private lateinit var sensorManager: SensorManager
-    private lateinit var accelerometer: Sensor
-    private var shakeSensor: ShakeSensor = ShakeSensor()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setFullScreen()
@@ -24,8 +14,6 @@ class MainActivity : AppCompatActivity(){
         val fragmentTransaction = supportFragmentManager.beginTransaction()
         fragmentTransaction.add(R.id.fragmentContainer, MainMenu())
         fragmentTransaction.commit()
-
-        setupShakeSensor()
     }
 
     private fun setFullScreen() {
@@ -33,29 +21,5 @@ class MainActivity : AppCompatActivity(){
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
             WindowManager.LayoutParams.FLAG_FULLSCREEN
         )
-    }
-
-    private fun setupShakeSensor() {
-        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
-        accelerometer  = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
-        shakeSensor.setOnShakeListener(object : OnShakeListener {
-            override fun onShake(count: Int) {
-                Toast.makeText(applicationContext, "Shake!", Toast.LENGTH_LONG).show()
-            }
-        })
-    }
-
-    override fun onResume() {
-        super.onResume()
-        sensorManager.registerListener(
-            shakeSensor,
-            accelerometer,
-            SensorManager.SENSOR_DELAY_UI
-        )
-    }
-
-    override fun onPause() {
-        sensorManager.unregisterListener(shakeSensor)
-        super.onPause()
     }
 }
--- app/src/main/java/com/example/virusgame/game/zombie/PreAttackZombie.kt
+++ app/src/main/java/com/example/virusgame/game/zombie/PreAttackZombie.kt
@@ -1,14 +1,31 @@
 package com.example.virusgame.game.zombie
 
+import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.Canvas
+import android.hardware.Sensor
+import android.hardware.SensorManager
 import com.example.virusgame.game.Clock
+import com.example.virusgame.game.ShakeSensor
 
 class PreAttackZombie(var zombie: Zombie) : ZombieState {
     private var frameNum: Int = 0
     private var lastFrameUpdateTime: Long = 0
     override val animation: List<Bitmap> = ZombieAnimations(zombie.context).preAttackAnimation1()
     private val startTime: Long = System.nanoTime()
+
+    private lateinit var sensorManager: SensorManager
+    private lateinit var accelerometer: Sensor
+    private var shakeSensor: ShakeSensor = ShakeSensor()
+
+    init {
+        setupShakeSensor()
+        sensorManager.registerListener(
+            shakeSensor,
+            accelerometer,
+            SensorManager.SENSOR_DELAY_UI
+        )
+    }
 
     override fun draw(canvas: Canvas, x: Float, y: Float) {
         canvas.drawBitmap(getAnimationFrame(), x, y, null)
@@ -30,7 +47,19 @@ class PreAttackZombie(var zombie: Zombie) : ZombieState {
 
     override fun update() {
         if(Clock().millisecondsHavePassed(startTime, 3000)){
+            sensorManager.unregisterListener(shakeSensor)
             zombie.state = AttackZombie(zombie)
         }
+    }
+
+    private fun setupShakeSensor() {
+        sensorManager = zombie.context.getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        accelerometer  = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+        shakeSensor.setOnShakeListener(object : ShakeSensor.OnShakeListener {
+            override fun onShake(count: Int) {
+                sensorManager.unregisterListener(shakeSensor)
+                zombie.state = AliveZombie(zombie)
+            }
+        })
     }
 }
