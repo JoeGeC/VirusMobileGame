0d714a655d5a5f0c3b0ef91edf36fc4da3668016
Joe <t7051015@live.tees.ac.uk>
2020-11-03 18:14:07 +0000

    Moved health UI back

--- app/src/main/java/com/example/virusgame/game/ui/HealthUi.kt
+++ app/src/main/java/com/example/virusgame/game/ui/HealthUi.kt
@@ -1,7 +1,8 @@
 package com.example.virusgame.game.ui
 
 import android.content.Context
-import android.graphics.*
+import android.graphics.Canvas
+import android.graphics.Paint
 import androidx.core.content.ContextCompat
 import com.example.virusgame.R
 import com.example.virusgame.game.vector2.FloatVector2
@@ -9,14 +10,6 @@ import com.example.virusgame.game.vector2.FloatVector2
 class HealthUi (context: Context, private val screenDimensions: FloatVector2, private val borderBottom: Float){
     private val healthPaint: Paint = Paint()
     private val healthLabelPaint: Paint = Paint()
-    private val healthBorder: Bitmap = BitmapFactory.decodeResource(context.resources, R.drawable.health_border)
-    private val healthBorderPos: FloatVector2 = FloatVector2(screenDimensions.x / 1.55f, borderBottom - borderBottom / 2.4f - healthBorder.height)
-    private val healthRect: Rect = Rect(
-        (healthBorderPos.x + healthBorder.width / 2.7f).toInt(),
-        (healthBorderPos.y + healthBorder.height / 4.0f).toInt(),
-        (healthBorderPos.x + healthBorder.width / 1.12f).toInt(),
-        (healthBorderPos.y + healthBorder.height / 1.4f).toInt()
-    )
 
     init{
         healthPaint.color = ContextCompat.getColor(context, R.color.green)
@@ -27,8 +20,9 @@ class HealthUi (context: Context, private val screenDimensions: FloatVector2, pr
     }
 
     fun draw(canvas: Canvas, currentHealth: Int, maxHealth: Int){
-        canvas.drawBitmap(healthBorder, healthBorderPos.x, healthBorderPos.y, null)
-        val healthBarStopPos = ((healthRect.right - healthRect.left) / maxHealth * currentHealth + healthRect.left).toFloat()
-        canvas.drawRect(healthRect.left.toFloat(), healthRect.top.toFloat(), healthBarStopPos, healthRect.bottom.toFloat(), healthPaint)
+        val healthBarStopPos = -borderBottom / maxHealth * currentHealth + borderBottom
+        canvas.drawRect(0.0f, healthBarStopPos, screenDimensions.x / 2.0f, borderBottom, healthPaint)
+        canvas.drawText("$currentHealth", screenDimensions.x / 4.5f, borderBottom - borderBottom / 1.7f, healthLabelPaint)
+        canvas.drawText("/ $maxHealth", screenDimensions.x / 4.5f, borderBottom - borderBottom / 2.7f, healthLabelPaint)
     }
 }
