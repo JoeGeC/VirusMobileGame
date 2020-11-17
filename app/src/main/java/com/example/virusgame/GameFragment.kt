33f459289ac78206f09f235c034aade2da82a16c
Joe <t7051015@live.tees.ac.uk>
2020-11-17 22:49:21 +0000

    Made layouts

--- app/src/main/java/com/example/virusgame/GameFragment.kt
+++ app/src/main/java/com/example/virusgame/GameFragment.kt
@@ -4,42 +4,44 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import androidx.fragment.app.Fragment
-import com.example.virusgame.game.GameView
 import com.example.virusgame.game.events.ShopOpensEvent
 import com.example.virusgame.settings.ClearDataListener
 import com.example.virusgame.settings.SettingsFragment
-import com.example.virusgame.shop.ShopFragment
+import kotlinx.android.synthetic.main.game.*
+import kotlinx.android.synthetic.main.speech.*
 
 
 class GameFragment : Fragment(), View.OnClickListener, WaveListener, ClearDataListener {
-    private lateinit var gameView: GameView
     private lateinit var menuFragmentManager: MenuFragmentManager
-    private lateinit var shopIcon: ShopIcon
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?) : View? {
-        val view = inflater.inflate(R.layout.game, container, false)
-        setupShopIcon(view)
-        setupGameView(view)
-        view.findViewById<ImageView>(R.id.settingsIcon).setOnClickListener(this)
-        menuFragmentManager = MenuFragmentManager(context!!, fragmentManager!!)
-        return view
+        return inflater.inflate(R.layout.game, container, false)
     }
 
-    private fun setupGameView(view: View) {
-        gameView = view.findViewById(R.id.gameView)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupShopIcon()
+        setupGameView()
+        settingsIcon.setOnClickListener(this)
+        speech.setOnClickListener(this)
+        menuFragmentManager = MenuFragmentManager(context!!, fragmentManager!!)
+    }
+
+    private fun setupGameView() {
         gameView.gameLoop.assignWaveListener(this)
     }
 
-    private fun setupShopIcon(view: View) {
-        shopIcon = view.findViewById(R.id.shopIcon)
+    private fun setupShopIcon() {
         shopIcon.setOnClickListener(this)
     }
 
     override fun onClick(view: View) {
-        if(view.id == R.id.shopIcon) shopIcon.onClick(menuFragmentManager, gameView.gameLoop)
-        if(view.id == R.id.settingsIcon) menuFragmentManager.openFragment(SettingsFragment(this, gameView.gameLoop))
+        when (view.id) {
+            R.id.shopIcon -> shopIcon.onClick(menuFragmentManager, gameView.gameLoop)
+            R.id.settingsIcon -> menuFragmentManager.openFragment(SettingsFragment(this, gameView.gameLoop))
+            R.id.speech -> speech.visibility = View.GONE
+        }
     }
 
     override fun onPause() {
