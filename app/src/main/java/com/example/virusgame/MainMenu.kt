99ba5b431604118cc2524eb0809fe7588a67fdc3
Joe <t7051015@live.tees.ac.uk>
2020-11-18 00:22:37 +0000

    Refactor

--- app/src/main/java/com/example/virusgame/MainMenu.kt
+++ app/src/main/java/com/example/virusgame/MainMenu.kt
@@ -4,22 +4,23 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.LinearLayout
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.example.virusgame.settings.ClearDataListener
 import com.example.virusgame.settings.SettingsFragment
+import kotlinx.android.synthetic.main.main_menu.*
 
 class MainMenu : Fragment(), ClearDataListener{
     private lateinit var  menuFragmentManager: MenuFragmentManager
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?) : View? {
-        val view = inflater.inflate(R.layout.main_menu, container, false)
-        view.findViewById<TextView>(R.id.playText).setOnClickListener{ openGame() }
-        view.findViewById<LinearLayout>(R.id.settingsIcon).setOnClickListener{ openSettings() }
+        return inflater.inflate(R.layout.main_menu, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        playText.setOnClickListener{ openGame() }
+        settingsIcon.setOnClickListener{ openSettings() }
         menuFragmentManager = MenuFragmentManager(context!!, fragmentManager!!)
-        return view
     }
 
     private fun openGame() {
