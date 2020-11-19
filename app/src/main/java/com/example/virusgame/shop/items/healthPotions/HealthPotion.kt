fe6198084cf73f90523b40086afe7a2be7e68741
Joe <t7051015@live.tees.ac.uk>
2020-11-19 22:35:43 +0000

    Added Spanish translations

--- app/src/main/java/com/example/virusgame/shop/items/healthPotions/HealthPotion.kt
+++ app/src/main/java/com/example/virusgame/shop/items/healthPotions/HealthPotion.kt
@@ -6,7 +6,7 @@ import com.example.virusgame.R
 class HealthPotion(override val context: Context): HealthItem() {
     override val imageResource: Int = R.drawable.green_potion_icon
     override val effect: Int = 50
-    override val description: String = context.getString(R.string.recovers) + " $effect " + context.getString(R.string.health).toLowerCase()
+    override val description: String = context.getString(R.string.recovers) + " $effect " + context.getString(R.string.health_recover)
     override val price: Int = 400
 
     init {
